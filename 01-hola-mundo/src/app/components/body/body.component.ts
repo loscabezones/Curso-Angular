8404bb9e4a879fcd9a0e86dab747cef3a098b9b5
USERSAD\loscabezones <david_gaia2@hotmail.com>
2018-11-14 17:28:54 +0100

    pruebas con ngif y ngfor en el body y creacion de componente footer con angular cli

--- 01-hola-mundo/src/app/components/body/body.component.ts
+++ 01-hola-mundo/src/app/components/body/body.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core'
 })
 export class BodyComponent {
 
+    mostrar:boolean = true;
+
+    frase:any ={
+        mensaje: 'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    };
+
+    personajes:string [] = ['Spiderman', 'Venom', 'Dr. Octopus'];
+
+
 }
