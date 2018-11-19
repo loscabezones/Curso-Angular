8178b9bf8d99086b3179d7095306f62ab5bd9eda
USERSAD\loscabezones <david_gaia2@hotmail.com>
2018-11-19 17:52:12 +0100

    creacion de nuevo componente heroe y desarollo devisualicacion de contenido

--- 02-spa/src/app/components/heroe/heroe.component.ts
+++ 02-spa/src/app/components/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from 'src/app/servicios/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe:any = {};
+  casa:string;
+
+  constructor( 
+    private _activatedRoute:ActivatedRoute,
+    private _heroesService:HeroesService ) {
+
+    this._activatedRoute.params.subscribe(params =>{
+      this.heroe = this._heroesService.getHeroe( params['id'] );
+      });
+
+   }
 
   ngOnInit() {
   }
--- 02-spa/src/app/servicios/heroes.service.ts
+++ 02-spa/src/app/servicios/heroes.service.ts
@@ -64,5 +64,9 @@ export class HeroesService {
     getHeroes():Heroe[]{
         return this.heroes;
     }
+
+    getHeroe(idx:number){
+      return this.heroes[idx];
+    }
     
 }
