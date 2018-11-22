0d89d0d2f43b0c1e024a100db7b384ec70c892f5
USERSAD\loscabezones <david_gaia2@hotmail.com>
2018-11-22 11:45:13 +0100

    Pype Json

--- 03-pipes/src/app/app.component.ts
+++ 03-pipes/src/app/app.component.ts
@@ -7,10 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  nombre:string = "David";
-  array:number[] = [1,2,3,4,5,6,7,8,9,10];
-  PI:number = Math.PI;
+  nombre: string = "David";
+  array: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
   a = 0.234;
-  salario:number = 1234.5;
+  salario: number = 1234.5;
+  heroe: object = {
+    nombre: "Logan",
+    clave: "Wolverine",
+    edad: 500,
+    direccion: {
+      calle: "Primera",
+      casa: "19"
+    }
+  }
 
 }
