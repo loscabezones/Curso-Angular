2cee007cf5adb3fbaebf8ef0f4d8a6b06788b0b6
loscabezones <david_gaia2@hotmail.com>
2018-11-30 12:36:42 +0100

    proyecto 05- preparado condos paginas pedientes y terminados

--- 05-deseos/src/app/app.module.ts
+++ 05-deseos/src/app/app.module.ts
@@ -3,21 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+//components Page
+import { PendientesPage } from '../pages/pendientes/pendientes.component';
+import { TerminadosPage } from '../pages/terminados/terminados.component';
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesPage,
+    TerminadosPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +27,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    PendientesPage,
+    TerminadosPage
   ],
   providers: [
     StatusBar,
---
+++ 05-deseos/src/pages/pendientes/pendientes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'page-pendientes',
+    templateUrl: 'pendientes.component.html'
+})
+export class PendientesPage {
+
+}
